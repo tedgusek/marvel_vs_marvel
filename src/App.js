74dee873890419c899da23e38a66216ce38a051c
Ted Gusek <theohgusek@gmail.com>
2023-09-27 10:40:51 -0400

    starting to style landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+import logo from './Marvel_Logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div id='marvelLogo'><img id='marvelLogo' src={logo} alt="logo" />VS<img id='marvelLogo' src={logo} alt="logo" /></div>
+        <br/>
+        <div id='mvm'>
+          <div>Marvel Component 1</div>
+          <div> VS </div>
+          <div>Marvel Component 2</div>
+        </div>
         <p>
           first commit Edit <code>src/App.js</code> and save to reload.
         </p>
